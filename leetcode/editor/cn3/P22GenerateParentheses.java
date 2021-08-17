dd144d0c142b42075879a8e3b2c6e3b26c49f6c4
baisiqi01 <baisiqi01@qiyi.com>
2021-08-17 20:30:14 +0800

    P22 add solution

--- leetcode/editor/cn3/P22GenerateParentheses.java
+++ leetcode/editor/cn3/P22GenerateParentheses.java
@@ -37,14 +37,64 @@ import java.util.List;
 //Java：括号生成
 public class P22GenerateParentheses {
     public static void main(String[] args) {
-        Solution solution = new Solution();
-        solution.generateParenthesis(3);
+        Solution22 solution = new Solution22();
+        System.out.println( solution.generateParenthesis(3));
+        Solution22_1 solution0 = new Solution22_1();
+        System.out.println(solution0.generateParenthesis(3));
+    }
+}
+
+//生成所有括号不考虑括号规则
+class Solution22_0 {
+    private List<String> res = new ArrayList<>();
+
+    public List<String> generateParenthesis(int n) {
+        helper(0, 2 * n, "");
+        return res;
+    }
+
+    private void helper(int level, int max, String s) {
+        //terminal
+        if (level >= max) {
+            res.add(s);
+            return;
+        }
+        //process
+        String s1 = s + '(';
+        String s2 = s + ')';
+        //drill down
+        helper(level + 1, max, s1);
+        helper(level + 1, max, s2);
+        //restore null
+    }
+
+}
+
+//left right代表左右括号剩余个数，注意右括号剩余必须大于左括号剩余才可添加！减少一个传参：不用传数量n参数
+class Solution22_1 {
+    private List<String> res = new ArrayList<>();
+
+    public List<String> generateParenthesis(int n) {
+        helper2(n, n, "");
+        return res;
+    }
+
+    private void helper2(int left, int right, String s) {
+        if (left == 0 && right == 0) {
+            res.add(s);
+            return;
+        }
+        //左括号只要有剩余就可以添加
+        if (left > 0) helper2(left - 1, right, s+'(');
+        //右括号剩余必须大于左括号剩余才可添加！！
+        if (right >0 && left < right) helper2(left, right - 1, s+')');
     }
 }
 
 //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
+class Solution22 {
     private List<String> res = new ArrayList<>();
+
     public List<String> generateParenthesis(int n) {
         _genertate(0, 0, n, "");
         return res;
